964d16c5fba24788720644dd74f09c4ff0174609
Elaidzha1940 <eli080293@gmail.com>
2024-03-22 04:28:41 +0300

    RotationApp + rotation3DEffect

--- 3DRotation/ContentView.swift
+++ 3DRotation/ContentView.swift
@@ -11,16 +11,21 @@ import SwiftUI
 
 struct ContentView: View {
     @State var open = false
-    @State var offset: CGFloat = 0
+    @State var offset: CGFloat = -UIScreen.main.bounds.height
     
     var body: some View {
         
         ZStack {
-           // View
+            // View
             Button(action: {
-                
+                self.offset = 0
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                    self.open.toggle()
+                }
             }, label: {
                 Text("Open")
+                    .font(.system(size: 20, weight: .bold, design: .monospaced))
+                    .foregroundStyle(Color(.label))
             })
             
             ZStack {
@@ -31,7 +36,10 @@ struct ContentView: View {
                         Spacer()
                         
                         Button(action: {
-                            
+                            self.open.toggle()
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                                self.offset = -UIScreen.main.bounds.height
+                            }
                         }, label: {
                             Image(systemName: "xmark.circle.fill")
                                 .font(.title)
@@ -39,13 +47,17 @@ struct ContentView: View {
                         })
                         .padding()
                     }
-                    
                     Spacer()
                 }
             }
             .cornerRadius(25)
             .padding()
+            .offset(y: self.offset)
+            .rotation3DEffect(.init(degrees: self.open ? 0 : 55),
+                              axis: (x: self.open ? 0 : 1.0, y: 0.0, z: 0.0))
+            .opacity(self.offset == -UIScreen.main.bounds.height ? 0 : 1.0)
         }
+        .animation(.spring(duration: 0.5))
     }
 }
 
